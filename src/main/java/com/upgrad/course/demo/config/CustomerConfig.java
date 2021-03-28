ccf70753fa93beac56dc8b94adb611729f2f43f9
Harish Singh <harish.singh89@gmail.com>
2021-03-28 16:41:24 +0530

    Adding logging aspect

--- src/main/java/com/upgrad/course/demo/config/CustomerConfig.java
+++ src/main/java/com/upgrad/course/demo/config/CustomerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import javax.sql.DataSource;
 
 @Configuration
+@EnableAspectJAutoProxy
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = {"com.upgrad.course.demo.repository"},
         entityManagerFactoryRef = "customerEntityManagerFactory")
